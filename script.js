7bcfb89430a0d2c68518f8f22e31ce4818f3909e
YeezyTheDev <adewoleoluwasegun7@gmail.com>
2026-05-29 14:35:54 +0100

    fixed image slider issues

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 const leftArrowButton = document.querySelector('.left-arrow');
 const rightArrowButton = document.querySelector('.right-arrow');
 const sliderWrapper = document.querySelector('.inner-slider')
+const imageSliderleft = document.querySelector('.left-image-button');
+const imageSliderright = document.querySelector('.right-image-button');
+
 console.log(sliderWrapper);
 let currentIndex = 0;
 const totalCards = 3;
@@ -22,3 +25,30 @@ leftArrowButton.addEventListener('click', function(){
         calShiftPosition();
     }
 })
+
+const slider = document.querySelector('.main-slider');
+const leftBtn = document.querySelector('.left-image-button');
+const rightBtn = document.querySelector('.right-image-button');
+
+let currentInde = 0;
+const totalSlides = document.querySelectorAll('.project').length;
+
+function updateSlider() {
+    slider.style.transform = `translateX(-${currentInde * 100}%)`;
+}
+
+// Right Button
+rightBtn.addEventListener('click', () => {
+    if (currentInde < totalSlides - 1) {
+        currentInde++;
+        updateSlider();
+    }
+});
+
+// Left Button
+leftBtn.addEventListener('click', () => {
+    if (currentInde > 0) {
+        currentInde--;
+        updateSlider();
+    }
+});
